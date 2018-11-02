4caacee379ec624885575f29964f6109a37e8701
Daniel Santos <daniel@brasileiro.ca>
2018-11-02 07:15:48 -0400

    🚀 Initial commit 🚀🚀

---
+++ app-server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+
+app.listen(3000);
+
+console.info('Polling server running');
