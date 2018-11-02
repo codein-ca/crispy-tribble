e658df7af5ffcf0b41e1d9e3ef49f37fc155b367
Daniel Santos <daniel@brasileiro.ca>
2018-11-02 09:22:35 -0400

    🚀 Adds Bootstrap css 🚀🚀

--- app-server.js
+++ app-server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
+// serves files on public folder (our web server root)
 app.use(express.static('./public'));
+// exposes direct access to B4 css/ and js/ folders
 app.use(express.static('./node_modules/bootstrap/dist'));
 
 app.listen(3000);
